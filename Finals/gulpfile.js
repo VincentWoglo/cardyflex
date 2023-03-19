32fcc7c41b03969b7fb1920f5ef69b8013519226
Vincent Woglo <vincentwoglo@gmail.com>
2023-03-19 09:19:49 -0600

    Login page

---
+++ Finals/gulpfile.js
@@ -0,0 +1,21 @@
+﻿const { series, watch, parallel } = require('gulp')
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+
+function Sass() {
+    return gulp.src('./wwwroot/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(csso())
+        .pipe(gulp.dest('./wwwroot/css'))
+}
+
+function WatchTask() {
+    watch('./wwwroot/scss/**/*.scss',
+        series(Sass))
+}
+
+exports.default = series(
+    parallel(Sass),
+    WatchTask
+)
